34df9577ebbf2e2b257b44e6b53686fe28b4324b
reddylaxman <2211cs010133@mallareddyuniversity.ac.in>
2024-03-12 18:24:28 +0530

    update changes

--- src/components/week4.js
+++ src/components/week4.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./week4.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Table from "react-bootstrap/Table";
+
 function UseStateExample() {
   const name = null;
   const rno = null;
@@ -33,57 +33,6 @@ function UseStateExample() {
         <h4>{details}</h4>
 
         <button onClick={getDetails}>Click to get Details</button>
-      </div>
-    </div>
-  );
-}
-function UseEffectExample() {
-  const headings = ["SNO", "Name of the student", "Roll No", "Email ID"];
-  const [Details, setDetails] = useState([]);
-  useEffect(() => {
-    const fetchStudentDetails = async () => {
-      const response = await fetch(
-        "https://sindhuravuri666.github.io/StudentList/StudentList.json"
-      );
-
-      const data = await response.json();
-
-      console.log(data);
-
-      setDetails(data);
-    };
-
-    fetchStudentDetails();
-  }, []);
-
-  return (
-    // className="component width"
-    // className="body-component-function"
-    <div>
-      <h4 style={{ textDecorationLine: "underline" }}>
-        Displaying Use effect example by fetching data
-      </h4>
-
-      <div className="table-width">
-        <Table striped bordered hover variant="danger">
-          <thead>
-            <tr>
-              {headings.map((h, index) => (
-                <th key={h}>{h}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {Details.slice(0, 5).map((student, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{student.Name}</td>
-                <td>{student.RollNo}</td>
-                <td>{student.EmailID}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
       </div>
     </div>
   );
@@ -164,8 +113,6 @@ function Week4() {
       <UseEffectExample2 />
       <hr />
       <UseEffectExample3 />
-      <hr />
-      <UseEffectExample />
     </div>
   );
 }
